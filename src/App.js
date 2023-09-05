ed027263b4f9bcf79ac895a448de93e874b822ce
Anajuliastradioto <anajuliastradioto@outlook.com>
2023-09-05 08:47:33 -0400

    Primeiro Projeto

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>src/App.js</code> Arquivo principal.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.todamateria.com.br/raizes-do-brasil/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leia Raízes do Brasil - Sérgio Buarque de Holanda
+        </a>
+      </header>
+    </div>
+
+    <div>
+      <p>Por Ana Júlia e Renata</p>
+    </div>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Topo.js
@@ -0,0 +1,11 @@
+import './Topo.css';
+
+function Topo() {
+  return (
+    <div className='Topo'>
+       <h1> Raízes do Brasil </h1>
+    </div>
+  );
+}
+
+export default Topo;
